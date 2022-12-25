35f9fac13524bab46240879c5898df81c8942c24
andhikayusup <andhikayusupm@gmail.com>
2022-12-25 22:59:50 +0700

    build: initialize mqtt publisher

---
+++ mqtt-publisher/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mqtt-publisher
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ mqtt-publisher/test/mqtt_publisher/core_test.clj
@@ -0,0 +1,7 @@
+(ns mqtt-publisher.core-test
+  (:require [clojure.test :refer :all]
+            [mqtt-publisher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
